d10a4b1d7887f295aabae1b574edaf1c541b802f
binbinbin13 <142992693+binbinbin13@users.noreply.github.com>
2024-02-20 00:40:40 +0800

    cc

---
+++ GIT_unityProj/Assets/Scripts/NextLevelScipt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelScipt : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
