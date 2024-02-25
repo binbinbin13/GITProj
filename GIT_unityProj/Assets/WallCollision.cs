8a5b7177361b76eceb5ac6622e07629a821cc646
H1kxru <suzhihao31@gmail.com>
2024-02-26 00:01:24 +0800

    Hell realm

---
+++ GIT_unityProj/Assets/WallCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Reduce player health when colliding with a wall
+            TakeDamage(10); // Adjust damage value as needed
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        // You can add further actions here like updating UI, checking for death, etc.
+        Debug.Log("Player health: " + currentHealth);
+    }
+
+
+}
